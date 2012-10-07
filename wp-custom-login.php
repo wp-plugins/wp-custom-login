f949bee87c16e4143e19c639fb03af7544b8f458
Ninos Ego <Ninos Ego@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-07 11:05:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-custom-login/trunk@609126 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-custom-login.php
+++ wp-custom-login.php
@@ -7,6 +7,9 @@ Version: 1.3.0
 Author URI: http://ninosego.de/
 */
 
+if( wp_custom_login_is_login_page() )
+	wp_enqueue_script('jquery');
+
 add_action( 'login_head', 'wp_custom_login_head_javascript' );
 function wp_custom_login_head_javascript() {
 ?>
@@ -29,4 +32,8 @@ function wp_custom_login_footer() {
 	do_action('wp_custom_login_footer_before');
 	get_footer();
 	do_action('wp_custom_login_footer_after');
+}
+
+function wp_custom_login_is_login_page() {
+	return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
 }
