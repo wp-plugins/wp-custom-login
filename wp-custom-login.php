67db2dbe61ebb91c853ebd85669b159298845990
Ninos Ego <Ninos Ego@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-01-24 17:37:14 +0000

    replaced the jQuery functions
    git-svn-id: https://plugins.svn.wordpress.org/wp-custom-login/tags/1.3.2@658171 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wp-custom-login.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Plugin Name: WP Custom Login
+Description: This plugin is adding the header and footer to the login page
+Author: Ninos Ego
+Version: 1.3.2
+Author URI: http://ninosego.de/
+*/
+
+add_action( 'login_head', 'wp_custom_login_head_javascript' );
+function wp_custom_login_head_javascript() {
+?>
+	<script type="text/javascript">
+		wp_custom_login_remove_element('wp-admin-css');
+		wp_custom_login_remove_element('colors-fresh-css');
+
+		function wp_custom_login_remove_element(id) {
+			var element = document.getElementById(id);
+			element.parentNode.removeChild(element);
+		}
+	</script>
+<?php
+}
+
+add_action( 'login_head', 'wp_custom_login_header' );
+function wp_custom_login_header() {
+	echo '<link rel="stylesheet" href="' . get_bloginfo('stylesheet_directory') . '/wp-custom-login.css" type="text/css" />';
+	do_action('wp_custom_login_header_before');
+	get_header();
+	do_action('wp_custom_login_header_after');
+}
+
+add_action( 'login_footer', 'wp_custom_login_footer' );
+function wp_custom_login_footer() {
+	do_action('wp_custom_login_footer_before');
+	get_footer();
+	do_action('wp_custom_login_footer_after');
+}
+
+function wp_custom_login_is_login_page() {
+	return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+}
