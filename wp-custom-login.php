702fa3c29c0351d12926042111ebcaa7f8093290
Ninos Ego <Ninos Ego@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-25 17:49:29 +0000

    WP 3.7 compatibility
    git-svn-id: https://plugins.svn.wordpress.org/wp-custom-login/trunk@793735 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-custom-login.php
+++ wp-custom-login.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Custom Login
 Description: This plugin is adding the header and footer to the login page
 Author: Ninos Ego
-Version: 1.4.2
+Version: 1.4.3
 Author URI: http://ninosego.de/
 */
 
